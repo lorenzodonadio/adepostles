7a017d1982db1d234e2c27d0d6f011ab893a1a13
lorenzodonadio <lorenzodonadio@hotmail.com>
2025-02-20 12:07:15 +0100

    ADD walltime

--- src/modmpi.f90
+++ src/modmpi.f90
@@ -3,6 +3,9 @@ module modmpi
    implicit none
    integer :: ierr, my_id, mpi_size
    logical :: is_root
+
+   real     :: CPU_program    !end time
+   real     :: CPU_program0   !start time
 contains
    subroutine init_mpi()
       call MPI_Init(ierr)
@@ -10,12 +13,20 @@ contains
       call MPI_Comm_size(MPI_COMM_WORLD, mpi_size, ierr)
 
       is_root = my_id == 0
-      print *, 'Hello from rank ', my_id, ' of ', mpi_size
 
+      if(myid==0)then
+         CPU_program0 = MPI_Wtime()
+         write(*,*) 'MPI mpi_size: ', mpi_size
+       end if
    end subroutine init_mpi
 
    subroutine exit_mpi()
-      call MPI_Finalize(ierr)
+      
+      if(myid==0)then
+         CPU_program = MPI_Wtime() - CPU_program0
+         write(6,*)'TOTAL wall time = ', CPU_program
+      end if
 
+      call MPI_Finalize(ierr)
    end subroutine exit_mpi
 end module modmpi
